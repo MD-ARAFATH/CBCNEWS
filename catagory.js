27e1c7bc2ea608e94a79de728004560f23c56967
MD-ARAFATH <mdarafath2500@gmail.com>
2024-04-06 03:03:48 +0600

    six commit

--- catagory.js
+++ catagory.js
@@ -39,3 +39,70 @@ const toggleSpinner = (isloading) => {
     loaderSection.classList.add("d-none");
   }
 };
+
+// function for show news
+function showNews(newses) {
+  let newsShort = newses.sort(
+    (a, b) => parseFloat(b.total_view) - parseFloat(a.total_view)
+  );
+  console.log(newsShort);
+  const mainNewsSection = document.getElementById("mainNewsSection");
+  mainNewsSection.classList.remove("d-none");
+  const faqSection = document.getElementById("faqSection");
+  faqSection.textContent = "";
+  const newsNumberField = document.getElementById("newsNumberField");
+  if (newses.length == 0) {
+    newsNumberField.innerText = `OPPS ! NO News found for this category`;
+  } else {
+    newsNumberField.innerText = `${newses.length} News found for this category`;
+  }
+  const newsBody = document.getElementById("newsBody");
+  newsBody.textContent = "";
+  newsShort.forEach((news) => {
+    const newsDiv = document.createElement("div");
+    newsDiv.classList.add("card", "mb-3");
+    newsDiv.innerHTML = `
+       <div class="row g-0">
+       <div class="col-md-3">
+         <img src="${news.thumbnail_url}" class="img-fluid rounded-start">  
+       </div>
+       <div class="col-md-9 ">
+         <div class="card-body m-0">
+           <h5 class="fs-3 fw-bold card-title">${news.title}</h5>
+           <p class="card-text mt-3">${news.details.slice(0, 300)}...</p>
+           <div class=" row  d-flex justify-content-between mt-4 " >
+             <div  class="col-md-3 d-flex align-items-center">
+               <img class="rounded w-5 "  src="${
+                 news.author.img
+               }" alt="" srcset="">
+               <div class="ps-3" >
+                 <h4 class="  " >${
+                   news.author.name == null
+                     ? "No name found"
+                     : news.author.name.slice(0, 13)
+                 }</h4>
+                 <p>12 Feb 2021</p>
+               </div>
+             </div>
+             <div  class="col-md-3 pt-3 ">
+               <h4 class="  ">👁‍🗨 ${
+                 news.total_view == null ? "No info" : news.total_view
+               }</h4>
+             </div>
+             <div  class="col-md-3 pt-3">
+               <h4 class="  ">☆☆☆☆</h4>
+             </div>
+             <button data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="newsDataId('${
+               news._id
+             }')" class=" col-md-2 btn btn-primary h-25 mt-4 " >Full News 🡢</button>
+           </div>
+           
+         </div>
+       </div>
+     </div>
+       `;
+    newsBody.appendChild(newsDiv);
+  });
+  // stop spinner
+  toggleSpinner(false);
+}
