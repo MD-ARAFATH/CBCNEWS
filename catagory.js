3be03df73c69ca3492004fc701b6fae655c7c76b
MD-ARAFATH <mdarafath2500@gmail.com>
2024-04-06 02:58:58 +0600

    four commit

--- catagory.js
+++ catagory.js
@@ -4,3 +4,18 @@ const loadCatagories = async () => {
   const data = await res.json();
   showcat(data.data.news_category);
 };
+
+loadCatagories();
+
+const showcat = (catagories) => {
+  const catContainer = document.getElementById("catContainer");
+
+  for (const catagori of catagories) {
+    let catName = catagori.category_name;
+    let catId = catagori.category_id;
+    const catLi = document.createElement("div");
+    catLi.classList.add("mb-3");
+    catLi.innerHTML = `<button  onclick="loadNews('${catagori.category_id}')" id="" class=" btn btn-light btn-sm text-decoration-none text-secondary fs-6" href="">${catName}</button>`;
+    catContainer.appendChild(catLi);
+  }
+};
